2cea2ed6edeff175a71a1cdac8fd4089dc2a5fb3
sandeep4ever <sandeepk2198@gmail.com>
2022-03-05 16:04:05 +0530

    firstcommit

---
+++ src/Component/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import pizzaImg from "../images/pizza_logo.png";
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          <img className="logo" src={pizzaImg} alt="logo" />
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/cart">
+                Cart
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/Component/Home.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+const Home = () => {
+  useEffect(() => {});
+  return <>Home</>;
+};
+
+export default Home;
