50534396dd5d5f1d3b2a77fb8005d37e9fcd4c8b
Niranjan Kumar <niranjan.kushwaha@mediassist.in>
2022-03-05 16:35:50 +0530

    added home folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./Component/Home";
+import Home from "./Component/Home/Home";
 import Header from "./Component/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Component/Footer";
--- src/Component/Home.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
-
-const Home = () => {
-  useEffect(() => {});
-  return <>Home</>;
-};
-
-export default Home;
